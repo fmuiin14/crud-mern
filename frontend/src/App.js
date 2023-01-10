0067af7d4f1039a684ea6e120174b5e434f092a7
fmuiin14 <fmuiin14@gmail.com>
2023-01-10 13:29:44 +0700

    add user

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,17 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import UserList from "./components/UserList";
+import AddUser from "./components/AddUser";
+
 function App() {
   return (
+    <BrowserRouter>
     <div className="container">
-      <UserList/>
+      <Routes>
+        <Route path="/" element={<UserList/>}/>
+        <Route path="/add" element={<AddUser/>}/>
+      </Routes>
     </div>
+    </BrowserRouter>
   );
 }
 
